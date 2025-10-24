5882a4b520e2347cd71e9fbd4c89676c39eb2337
Goofy.L <goofy.l@GoofyL-H-23PC-MP16-420.local>
2025-10-24 17:40:28 +0900

    Car 클래스에 속성과 메서드 추가

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -1,4 +1,17 @@
 package org.example;
 
 public class Car {
+    public Integer speed;
+    public String model;
+    public String color;
+
+    public Car(Integer speed, String model, String color) {
+        this.speed = speed;
+        this.model = model;
+        this.color = color;
+    }
+
+    public void move() {
+        System.out.println("The car is moving at " + speed + " km/h.");
+    }
 }
